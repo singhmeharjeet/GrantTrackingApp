346c1c6e534cb458aac2162bab7a8c03503c00e6
tjuneja <tarunjeev_singh_juneja@sfu.ca>
2022-10-22 22:37:58 -0700

    pushing 2nd time

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import Header from "./Components/Header/Header";
+import Add from "./Components/Add/Add.js";
 function App() {
   return (
     <div className="App">
       <Header />
+      <Add />
       <div className="container"></div>
     </div>
   );
---
+++ client/src/Components/Add/Add.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Add.css";
+function Add() {
+  return (
+    <div>
+      <h1 className="add">Add record</h1>
+    </div>
+  );
+}
+
+export default Add;
